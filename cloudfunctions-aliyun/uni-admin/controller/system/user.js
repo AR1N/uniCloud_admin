c93375531e2d7db000f385de833320543c7d290a
aren <739190475@qq.com>
2020-12-04 17:26:24 +0800

    first commit

---
+++ cloudfunctions-aliyun/uni-admin/controller/system/user.js
@@ -0,0 +1,39 @@
+const {
+	Controller
+} = require('uni-cloud-router')
+const uniID = require('uni-id')
+module.exports = class UserController extends Controller {
+	async addUser() {
+		const {
+			username,
+			password,
+			role,
+			mobile,
+			email,
+			status
+		} = this.ctx.data
+
+		const result = await uniID.register({
+			username,
+			password,
+			role,
+			mobile,
+			email,
+			status
+		})
+		delete result.token
+		delete result.tokenExpired
+
+		return result
+	}
+
+	async updateUser() {
+	    const data = this.ctx.data
+
+		const result = await uniID.updateUser({
+	        ...data
+	    })
+
+		return result
+	}
+}
